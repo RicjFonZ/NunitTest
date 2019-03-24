74ee18c6f8b34a6a6e417cf736cf3b8829bdc87e
Ricardo Fonseca <ricardo.jfonseca@outlook.com>
2019-03-24 17:45:20 +0000

    First commit

---
+++ Test/UnitTest/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.UnitTest
+{
+    [TestFixture]
+    class Test
+    {
+
+        [Test]
+        public void test()
+        {
+
+
+            Assert.AreEqual(1,1);
+
+        }
+
+
+    }
+}
